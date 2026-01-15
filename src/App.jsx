5dc54f04c404fbe4ec18533338d8a6cd5bb63c94
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2026-01-14 22:34:59 -0300

    create /task route

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
-import { useState } from "react"; // Adicione esta linha
+import { useEffect, useState } from "react";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { v4 } from "uuid";
 
 function App() {
   // State (estado)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description:
-        "Estudar programação para se tornar um desenvolver full stack",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar inglês",
-      description: "Estudar inglês para se tornar fluente",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar matemática",
-      description:
-        "Estudar matemática para se tornar um desenvolvedor full stack",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
-      if (task.id == taskId) {
+      if (task.id === taskId) {
         return { ...task, isCompleted: !task.isCompleted };
       }
 
       return task;
     });
+
     setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
   }
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,41 @@
-import { ChevronRightIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
-  console.log(props);
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
-    <ul className="space-y-4 p-6 bg-slate-200 roundend-md shadow">
-      {props.tasks.map((task) => (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => {
+              onSeeDetailsClick(task);
+            }}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
+          </button>
+          <button
+            className="bg-slate-400 p-2 rounded-md text-white"
+            onClick={() => onDeleteTaskClick(task.id)}
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
